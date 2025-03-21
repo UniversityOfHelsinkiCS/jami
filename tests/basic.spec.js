558297857e6db727f7241cc3512add8854c72b66
Veikkosuhonen <vesuvesuh@gmail.com>
2025-03-21 14:17:18 +0200

    test: Add test for employee special group

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -20,3 +20,26 @@ test('User with no iam groups gets no access', async () => {
     specialGroup: {},
   })
 })
+
+test('User with employee iam gets employee special group', async () => {
+  const res = await api.post('', {
+    userId: 'user-1',
+    iamGroups: ['hy-employees'],
+  })
+
+  expect(res.status).toBe(200)
+  expect(await res.json()).toEqual({
+    specialGroup: {
+      employee: true,
+    },
+  })
+})
+
+test('User without employee iam gets no special groups', async () => {
+  const res = await api.post('', { userId: 'user-1', iamGroups: ['hy-pelle'] })
+
+  expect(res.status).toBe(200)
+  expect(await res.json()).toEqual({
+    specialGroup: {},
+  })
+})
