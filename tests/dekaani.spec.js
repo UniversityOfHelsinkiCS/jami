730409b982a7e4a2bacb31b0e23a3376df2a61e5
outisa <savolainen.outi@gmail.com>
2026-09-21 00:56:50 +0300

    Fix prettier and lint

--- tests/dekaani.spec.js
+++ tests/dekaani.spec.js
@@ -11,20 +11,21 @@ describe.concurrent('Dekaani', () => {
     expect(res.status).toBe(200)
     const access = await res.json()
 
-      // expect(access).toHaveProperty('H20') // TODO: dekaani not getting access to faculty
+    // expect(access).toHaveProperty('H20') // TODO: dekaani not getting access to faculty
 
-      // These are the programmes that oikis dekaani should have access to
-      ;[
-        'KH20_001', // oikeustieteellinen
-        'MH20_001',
-        'MH20_002',
-        'MH20_003',
-        'T920102',
-      ].forEach((programme) => {
-        expect(access[programme].read).toBe(true)
-        expect(access[programme].write).toBeFalsy()
-        expect(access[programme].admin).toBeFalsy()
-      })
+    // These are the programmes that oikis dekaani should have access to
+    ;[
+      'KH20_001', // oikeustieteellinen
+      'MH20_001',
+      'MH20_002',
+      'MH20_003',
+      'T920102',
+    ].forEach((programme) => {
+      console.log('wtf')
+      expect(access[programme].read).toBe(true)
+      expect(access[programme].write).toBeFalsy()
+      expect(access[programme].admin).toBeFalsy()
+    })
   })
 
   it('opetusvaradekaani gets ADMIN access to faculty and each programme of faculty', async () => {
@@ -36,20 +37,20 @@ describe.concurrent('Dekaani', () => {
     expect(res.status).toBe(200)
     const access = await res.json()
 
-      // expect(access).toHaveProperty('H20') // TODO: dekaani not getting access to faculty
+    // expect(access).toHaveProperty('H20') // TODO: dekaani not getting access to faculty
 
-      // These are the programmes that oikis dekaani should have access to
-      ;[
-        'KH20_001', // oikeustieteellinen
-        'MH20_001',
-        'MH20_002',
-        'MH20_003',
-        'T920102',
-      ].forEach((programme) => {
-        expect(access[programme].read).toBe(true)
-        expect(access[programme].write).toBe(true)
-        expect(access[programme].admin).toBe(true)
-      })
+    // These are the programmes that oikis dekaani should have access to
+    ;[
+      'KH20_001', // oikeustieteellinen
+      'MH20_001',
+      'MH20_002',
+      'MH20_003',
+      'T920102',
+    ].forEach((programme) => {
+      expect(access[programme].read).toBe(true)
+      expect(access[programme].write).toBe(true)
+      expect(access[programme].admin).toBe(true)
+    })
   })
 
   it('R K-T works', async () => {
@@ -68,22 +69,12 @@ describe.concurrent('Dekaani', () => {
     expect(res.status).toBe(200)
     const access = await res.json()
 
-    ;[
-      'KH90_001',
-      'MH90_001',
-      'T921108',
-      'T922106'
-    ].forEach((programme) => {
+    ;['KH90_001', 'MH90_001', 'T921108', 'T922106'].forEach((programme) => {
       expect(access[programme].read).toBe(true)
       expect(access[programme].write).toBe(true)
       expect(access[programme].admin).toBe(true)
     })
-
-    ;[
-      'KH10_001',
-      'MH30_005',
-      'T920102'
-    ].forEach((programme) => {
+    ;['KH10_001', 'MH30_005', 'T920102'].forEach((programme) => {
       expect(access[programme].read).toBe(true)
       expect(access[programme].write).toBeFalsy()
       expect(access[programme].admin).toBeFalsy()
